ed78152f7698805ee49d1fbe7576b271e5741ed1
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-11-11 23:19:44 +0200

    Add Axios interceptors / refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://drf-api-backend-11522e18c941.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'; // images and more
 axios.defaults.withCredentials = true; // required for cookies
+
+export const axiosReq = axios.create(); // requests
+export const axiosRes = axios.create(); // responses
