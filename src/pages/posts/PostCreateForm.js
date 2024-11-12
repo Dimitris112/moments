2daca7ff6f954e2d69e9399a2be0adfe17ac95b3
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-11-12 11:48:35 +0200

    Send post data to the API

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,18 +1,23 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
+
+import Asset from "../../components/Asset";
 
 import Upload from "../../assets/upload.png";
 
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import Asset from "../../components/Asset";
-import { Image } from "react-bootstrap";
+
+import { useHistory } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostCreateForm() {
     const [errors, setErrors] = useState({});
@@ -24,6 +29,9 @@ function PostCreateForm() {
     });
     const { title, content, image } = postData;
 
+    const imageInput = useRef(null);
+    const history = useHistory();
+
     const handleChange = (event) => {
         setPostData({
             ...postData,
@@ -32,16 +40,32 @@ function PostCreateForm() {
     };
 
     const handleChangeImage = (event) => {
-        // https://developer.mozilla.org/en-US/docs/Web/API/URL/revokeObjectURL_static - revokeObjectURL - revokes an object URL that was created with URL.createObjectURL()
-        // https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL_static - creates an object URL from a Blob, File or DOMString
-        // https://developer.mozilla.org/en-US/docs/Web/API/URL
-
         if (event.target.files.length) {
             URL.revokeObjectURL(image);
             setPostData({
                 ...postData,
-                image: URL.createObjectURL(event.target.files[0]), // [0] just to choose the first one
+                image: URL.createObjectURL(event.target.files[0]),
             });
+        }
+    };
+
+    const handleSubmit = async (event) => {
+        // https://developer.mozilla.org/en-US/docs/Web/API/FormData - formData is a class that provides a way to easily construct a set of key/value pairs representing form fields and their values, which can then be easily transmitted using the fetch API
+        event.preventDefault();
+        const formData = new FormData();
+
+        formData.append("title", title);
+        formData.append("content", content);
+        formData.append("image", imageInput.current.files[0]);
+
+        try {
+            const { data } = await axiosReq.post("/posts/", formData);
+            history.push(`/posts/${data.id}`);
+        } catch (err) {
+            console.log(err);
+            if (err.response?.status !== 401) {
+                setErrors(err.response?.data);
+            }
         }
     };
 
@@ -56,6 +80,12 @@ function PostCreateForm() {
                     onChange={handleChange}
                 />
             </Form.Group>
+            {errors?.title?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
             <Form.Group>
                 <Form.Label>Content</Form.Label>
                 <Form.Control
@@ -66,10 +96,15 @@ function PostCreateForm() {
                     onChange={handleChange}
                 />
             </Form.Group>
+            {errors?.content?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
 
             <Button
                 className={`${btnStyles.Button} ${btnStyles.Blue}`}
-                onClick={() => { }}
+                onClick={() => history.goBack()}
             >
                 cancel
             </Button>
@@ -80,7 +115,7 @@ function PostCreateForm() {
     );
 
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Row>
                 <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
                     <Container
@@ -108,7 +143,7 @@ function PostCreateForm() {
                                 >
                                     <Asset
                                         src={Upload}
-                                        message="Click to upload an image"
+                                        message="Click or tap to upload an image"
                                     />
                                 </Form.Label>
                             )}
@@ -117,8 +152,15 @@ function PostCreateForm() {
                                 id="image-upload"
                                 accept="image/*"
                                 onChange={handleChangeImage}
+                                ref={imageInput}
                             />
                         </Form.Group>
+                        {errors?.image?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
+
                         <div className="d-md-none">{textFields}</div>
                     </Container>
                 </Col>
