c9410d05b918428ae01c47068569a80353ec6415
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-11-13 18:14:06 +0200

    Add NavBar.test.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,13 +2,13 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-import { setupServer } from 'msw/node'
-import { handlers } from './mocks/handlers'
+import "@testing-library/jest-dom";
+import { setupServer } from "msw/node";
+import { handlers } from "./mocks/handlers";
 
 // through spread handlers array -> call server listen before all tests -> reset handlers after each test -> close server after all tests
-const server = setupServer(...handlers)
+const server = setupServer(...handlers);
 
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
