d3439355aa7923c5994ce646abab014eba99183d
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-11-13 17:05:58 +0200

    Add unfollow funcionality - feed page page responds

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,15 +1,15 @@
 import { axiosReq } from "../api/axiosDefaults";
 
-export const fetchMoreData = async (resource, setResource) => { // async resource -> can be re used with any paginated data like comments / profiles etc
+export const fetchMoreData = async (resource, setResource) => {
     try {
         const { data } = await axiosReq.get(resource.next);
         setResource((prevResource) => ({
             ...prevResource,
-            next: data.next, // fetch next page
-            results: data.results.reduce((acc, cur) => { // reduce to look through the new page of results that got fetched through the API
-                return acc.some((accResult) => accResult.id === cur.id) // some method -> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
-                    ? acc // if some -> true, return accumulator without the post to it
-                    : [...acc, cur]; // else return an array with spread accumulator with the new post added to it
+            next: data.next,
+            results: data.results.reduce((acc, cur) => {
+                return acc.some((accResult) => accResult.id === cur.id)
+                    ? acc
+                    : [...acc, cur];
             }, prevResource.results),
         }));
     } catch (err) { }
@@ -28,6 +28,24 @@ export const followHelper = (profile, clickedProfile, following_id) => {
             ? // This is the profile of the logged in user
             // update its following count
             { ...profile, following_count: profile.following_count + 1 }
+            : // this is not the profile the user clicked on or the profile
+            // the user owns, so just return it unchanged
+            profile;
+};
+
+export const unfollowHelper = (profile, clickedProfile) => {
+    return profile.id === clickedProfile.id
+        ? // This is the profile I clicked on,
+        // update its followers count and set its following id
+        {
+            ...profile,
+            followers_count: profile.followers_count - 1,
+            following_id: null,
+        }
+        : profile.is_owner
+            ? // This is the profile of the logged in user
+            // update its following count
+            { ...profile, following_count: profile.following_count - 1 }
             : // this is not the profile the user clicked on or the profile
             // the user owns, so just return it unchanged
             profile;
